25e5ab9aedd635218874115fba399aa90d52b482
LSE <60775325+lse-odoo@users.noreply.github.com>
2021-10-22 15:44:33 +0200

    Update models.py

--- custom_module/models/models.py
+++ custom_module/models/models.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import numpy
-# from odoo import models, fields, api
+import numpy as np
+from odoo import models, fields, api
 
 
-# class custom_module(models.Model):
-#     _name = 'custom_module.custom_module'
-#     _description = 'custom_module.custom_module'
+class custom_module(models.Model):
+    _name = 'custom_module.custom_module'
+    _description = 'custom_module.custom_module'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
+            a = np.arange(15).reshape(3, 5)
+            print(a.shape)
